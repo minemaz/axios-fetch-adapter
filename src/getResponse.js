64e2bff3feab2ce84190d246606a4fb2d907d038
Hiroki Minematsu <mine@lancard.com>
2022-02-28 16:09:24 +0900

    Instead of fetch Change to use cordova-plugin-advanced-http

--- src/getResponse.js
+++ src/getResponse.js
@@ -1,4 +1,5 @@
 import createError from 'axios/lib/core/createError';
+import buildFullPath from 'axios/lib/core/buildFullPath';
 
 /**
  * Fetch API stage two is to get response body. This funtion tries to retrieve
@@ -7,38 +8,42 @@ import createError from 'axios/lib/core/createError';
 export default async function getResponse(request, config) {
     let stageOne;
     try {
-        stageOne = await fetch(request);
+      stageOne = await new Promise((res, rej) => {
+        let options = config;
+        if (!options.hasOwnProperty('headers')) {
+          options = Object.assign(options, { "headers": {}});
+        }
+        if (!options.hasOwnProperty('data')) {
+          options = Object.assign(options, { "data": {}});
+        }
+        options.url = buildFullPath(options.baseURL, options.url);
+        if (options.method === 'post') {
+          if (options.headers['Content-Type'] === 'application/json') {
+            cordova.plugin.http.setDataSerializer('json');
+            options.data = JSON.parse(options.data);
+          } else if (options.headers['Content-Type'].match(/^multipart\/form-data;/)){
+            cordova.plugin.http.setDataSerializer('multipart');
+          } else {
+            cordova.plugin.http.setDataSerializer('urlencoded');
+          }
+        }
+        cordova.plugin.http.sendRequest(options.url, options, res, rej);
+      });
     } catch (e) {
-        return Promise.reject(createError('Network Error', config, null, request));
+        return Promise.reject(createError(JSON.stringify(e), config, null, request));
     }
-
     const response = {
-        ok: stageOne.ok,
+        // ok: stageOne.ok,
         status: stageOne.status,
         statusText: stageOne.statusText,
-        headers: new Headers(stageOne.headers), // Make a copy of headers
+        // headers: new Headers(stageOne.headers), // Make a copy of headers
+        headers: stageOne.headers, // Make a copy of headers
         config: config,
         request,
     };
 
     if (stageOne.status >= 200 && stageOne.status !== 204) {
-        switch (config.responseType) {
-            case 'arraybuffer':
-                response.data = await stageOne.arrayBuffer();
-                break;
-            case 'blob':
-                response.data = await stageOne.blob();
-                break;
-            case 'json':
-                response.data = await stageOne.json();
-                break;
-            case 'formData':
-                response.data = await stageOne.formData();
-                break;
-            default:
-                response.data = await stageOne.text();
-                break;
-        }
+      response.data = stageOne.data;
     }
 
     return Promise.resolve(response);
